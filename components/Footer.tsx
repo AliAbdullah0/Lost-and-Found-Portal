33af544c26db16b96879df7893f15beb2b4908c6
Ali Abdullah <aliabdullah10nov2006@gmail.com>
2026-04-04 22:39:38 +0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,10 +4,7 @@ const Footer = () => {
       
       <div className="max-w-7xl mx-auto px-5 py-6 flex md:flex-col flex-col-reverse md:flex-row items-center justify-between gap-4">
     <div className="space-y-2">
-        <ul className="text-gray-600 text-sm">
-            <li><span className="font-semibold">Ali Abdullah : </span>01-136252-010</li>
-            <li><span className="font-semibold">Wasay Goraya : </span>01-136252-048</li>
-        </ul>
+
         <p className="md:text-sm text-xs text-gray-500 ">
           © {new Date().getFullYear()} Lost & Found Portal. All rights reserved.
         </p>
